fd8f0b8fd05bf30fc7c4c400aa8a9c0a66002f19
Luis Rodriguez <luishi.rodriguez@gmail.com>
2023-08-28 15:21:34 -0300

    Boton de turnos y desplazamiento de imagenes

--- codigo/script.js
+++ codigo/script.js
@@ -1,10 +1,35 @@
+//Ampliar imagenes
 const galleryImages = document.querySelectorAll(".trabajos-img");
 const overlay = document.getElementById("overlay");
 const overlayImage = document.getElementById("overlayImage");
 
-galleryImages.forEach((image) => {
+//Desplazarse por las imagenes
+
+const antesButton = document.querySelector(".antes");
+const despuesButton = document.querySelector(".despues");
+
+let currentImageIndex = 0;
+
+function mostrarImagenEnOverlay(index) {
+  overlayImage.src = galleryImages[index].src;
+}
+
+antesButton.addEventListener("click", () => {
+  event.stopPropagation();
+  currentImageIndex = (currentImageIndex - 1 + galleryImages.length) % galleryImages.length;
+  mostrarImagenEnOverlay(currentImageIndex);
+});
+
+despuesButton.addEventListener("click", () => {
+  event.stopPropagation();
+  currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
+  mostrarImagenEnOverlay(currentImageIndex);
+});
+
+galleryImages.forEach((image, index) => {
   image.addEventListener("click", () => {
-    overlayImage.src = image.src;
+    currentImageIndex = index;
+    mostrarImagenEnOverlay(currentImageIndex);
     overlay.classList.add("active");
   });
 });
@@ -12,6 +37,8 @@ galleryImages.forEach((image) => {
 overlay.addEventListener("click", () => {
   overlay.classList.remove("active");
 });
+
+//Boton del menu desplegable
 
 const botonResponsive = document.querySelector(".boton-responsive");
 const navUl = document.querySelector(".nav__ul");
